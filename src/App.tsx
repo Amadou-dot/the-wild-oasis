c282eaacb756d5d73d99235f27c6e715e1728ae5
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2024-12-25 00:25:00 -0700

    Add routing with react-router-dom and refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,31 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import NewUsers from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
 
-const StyledApp = styled.div`
-  padding: 20px;
-  min-height: 100vh;
-`;
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type='horizontal'>
-            <Heading as='h1'>The wild Oasis</Heading>
-            <div>
-              <Heading as='h2'>Check it rrout</Heading>
-              <Button>Check in</Button>
-              <Button variation="danger" size="medium">Check out</Button>
-            </div>
-          </Row>
-          <Row>
-            <Heading as='h3'>Form</Heading>
-            <form action=''>
-              <Input type='number' placeholder='Search' />
-              <Input type='number' placeholder='Search' />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to={'/dashboard'} />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/bookings' element={<Bookings />} />
+          <Route path='/cabins' element={<Cabins />} />
+          <Route path='/users' element={<NewUsers />} />
+          <Route path='/settings' element={<Settings />} />
+          <Route path='/account' element={<Account />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/pages/Bookings.tsx
+++ src/pages/Bookings.tsx
@@ -1,13 +1,11 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
 
-function Bookings() {
+export default function Bookings() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All bookings</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>All bookings</Heading>
       <p>TEST</p>
     </Row>
   );
 }
-
-export default Bookings;
